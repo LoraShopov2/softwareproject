82bc4dd26a8db5e917898cec4ce3e5b39d0bfd67
daniella1sim <simonovsky1@mail.tau.ac.il>
2024-07-09 13:09:19 +0300

    ggggg

--- hw1.c
+++ hw1.c
@@ -1,10 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int D;
-int K;
-int N;
-typedef double coordinats[D];
+
+
+#define MAX_LINE_LENGTH 1000
+
+
+int main(int argc, char *argv[]) {
+    int K, iter, N, D;
+    float **data;
+    
+    if (argc < 3 || argc > 4) {
+        printf("An error has occurred!\n");
+        return -1;
+    }
+    int K = atoi(argv[1]);
+    if (argc == 4) iter = atoi(argv[2]);
+    else iter = 200;
+    
+    if (iter <=0 || iter >= 1000){
+        printf("Invalid maximum iteration!\n");
+        return -1;
+    }
+    
+    file_parse(stdin, &data, &N, &D);
+
+    if (K <= 0 || K > N){
+        printf("Invalid number of clusters\n");
+        return -1;
+    }
+
+
+
+    typedef double coordinats[D];
+    
+}
+
+
+
+int file_parse(FILE *file, float ***array, int *N, int *D){
+    int row = 0, col = 0, SizeOfArray = 10;
+    char line[MAX_LINE_LENGTH];
+    int i = 0;
+
+    // initial allocation of memory
+    *array = (float **)malloc(SizeOfArray * sizeof(float *));
+
+    while(1){
+        int ch = getchar();
+        if (ch == ',' || ch == ' ' || ch == '\n' || ch == EOF) {
+            if (i > 0){
+                line[i] = '\0';
+                
+                //memory allocation for mew column
+                if (col == 0){
+                    (*array)[row] = (float *)malloc((*D) * sizeof(float));
+                }
+                (*array)[row][col++] = atof(line);
+                i = 0;
+            }
+            if (ch == '\n' || ch == EOF) {
+                if (col > 0) {
+                    row++;
+                    if (row >= SizeOfArray) {
+                        SizeOfArray *= 2;
+                        *array = (float **)realloc(*array, SizeOfArray * sizeof(float *));
+                    }
+                    col = 0;
+                }
+                if (ch == EOF) break;
+            }
+        } else {
+            line[i++] = ch;
+        }
+    }
+    *N = row;
+    return;
+}
+
 
 // Define the node structure
 typedef struct {
